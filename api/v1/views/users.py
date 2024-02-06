7ef569816e3a1f5305a1450876aab5b05e74585a
= <aleksifuna@gmail.com>
2024-02-06 23:46:48 +0000

    disabled strict slashes directly on the routes

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -6,7 +6,7 @@ from models import storage
 from models.user import User
 
 
-@app_views.route('/users', methods=['GET'])
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
 def get_users():
     """Returns a list of all user objects"""
     users = storage.all(User).values()
@@ -14,7 +14,11 @@ def get_users():
     return jsonify(user_list)
 
 
-@app_views.route('/users/<user_id>', methods=['GET'])
+@app_views.route(
+        '/users/<user_id>',
+        methods=['GET'],
+        strict_slashes=False
+        )
 def get_user(user_id):
     """Return a dict representation of user object"""
     user = storage.get(User, user_id)
@@ -24,7 +28,11 @@ def get_user(user_id):
         abort(404)
 
 
-@app_views.route('/users/<user_id>', methods=['DELETE'])
+@app_views.route(
+        '/users/<user_id>',
+        methods=['DELETE'],
+        strict_slashes=False
+        )
 def delete_user(user_id):
     """Delete a user object"""
     user = storage.get(User, user_id)
@@ -36,7 +44,11 @@ def delete_user(user_id):
         abort(404)
 
 
-@app_views.route('/users', methods=['POST'])
+@app_views.route(
+        '/users',
+        methods=['POST'],
+        strict_slashes=False
+        )
 def create_user():
     """Creates a user object"""
     if request.json:
@@ -52,7 +64,10 @@ def create_user():
         return jsonify({"error": "Not a JSON"}), 400
 
 
-@app_views.route('/users/<user_id>', methods=['PUT'])
+@app_views.route(
+        '/users/<user_id>',
+        methods=['PUT'],
+        strict_slashes=False)
 def update_user(user_id):
     """updates user object"""
     user = storage.get(User, user_id)
